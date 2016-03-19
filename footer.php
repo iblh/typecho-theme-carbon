17d777fcc01e12b4a041b3ccf41ff89f26657773
viosey <viosey@outlook.com>
2016-03-19 16:44:51 +0800

    1.0.0

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
                 </li>
             <?php endwhile; ?>
             <li><a href="<?php $this->options->feedUrl(); ?>" class="out-site">RSS</a></li>
-            <li><a href="#" target="_blank" class="out-site">Theme.Carbon</a></li>
+            <li><a href="https://github.com/viosey/Carbon" target="_blank" class="out-site">Theme.Carbon</a></li>
             <li><a href="http://typecho.org" target="_blank" class="out-site">Typecho</a></li>
         </ul>
         </nav>
